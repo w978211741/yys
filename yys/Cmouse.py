ce640ad0fb4aa26ea201f5cf372fbe3311d06af6
w978211741 <297786304@qq.com>
2019-03-06 08:03:59 +0800

    rte

--- yys/Cmouse.py
+++ yys/Cmouse.py
@@ -17,7 +17,17 @@ class cmouse:
         #self.m.click(x,y,1,1)
         self.m.click(x, y, 1, 2)
 
-    #发送esc退出事件
+    # 发送esc退出事件
     def sendEsc(self,hwnd):
         win32api.SendMessage(hwnd, win32con.WM_KEYDOWN, win32con.VK_ESCAPE, 0)
         win32api.SendMessage(hwnd, win32con.WM_KEYUP, win32con.VK_ESCAPE, 0)
+
+    def gun_lun(self, n, m):
+        self.m.scroll(n, m)
+
+    def vertical_tuo(self, x, y, n):
+        self.m.press(x, y, 1)
+        self.m.move(x, y - n)
+        self.m.release(x, y, 1)
+
+
